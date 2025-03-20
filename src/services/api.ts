9ef54bac228355b18e8a113a81209038462fe57b
yusef frz <y.farzin80@gmail.com>
2025-03-20 17:28:38 +0330

    Set up environment variables for Vite

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const client = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL
+    baseURL: apiUrl
 })
 
 
